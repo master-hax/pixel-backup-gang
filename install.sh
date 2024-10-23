ca5366cc3b800045fd733b370419c8dc5b1cebde
Vivek Revankar <vivek@master-hax.com>
2024-10-22 19:07:25 -0700

    don't use set

--- install.sh
+++ install.sh
@@ -6,14 +6,13 @@
 # Usage: sh -c "$(curl -fSs https://raw.githubusercontent.com/master-hax/pixel-backup-gang/master/install.sh)"
 ################################################################################
 
-set -eu
 pbg_tarball_version="0.0.2"
 pbg_tarball_filename="pixel-backup-gang-$pbg_tarball_version.tar.gz"
 pbg_tarball_url="https://github.com/master-hax/pixel-backup-gang/releases/download/$pbg_tarball_version/$pbg_tarball_filename"
 echo "install.sh: downloading version $pbg_tarball_version from github"
-curl -fL $pbg_tarball_url --output $pbg_tarball_filename
-echo "install.sh: unpacking the release archive (requires root)"
-su --command "tar -xvf $pbg_tarball_filename"
-echo "install.sh: make the release executable (requires root)"
-su --command "chmod +x ./pixel-backup-gang/*.sh"
+curl -fL $pbg_tarball_url --output $pbg_tarball_filename || { echo 'failed to download release archive' ; exit 1; }
+echo "install.sh: unpacking the release archive (requires root)" || { echo 'failed to unpack release archive' ; exit 1; }
+su --command "tar -xvf $pbg_tarball_filename" || { echo 'failed to inflate release archive' ; exit 1; }
+echo "install.sh: making the release executable (requires root)"
+su --command "chmod +x ./pixel-backup-gang/*.sh" || { echo 'failed to make scripts executable' ; exit 1; }
 echo "install.sh: pixel backup gang successfully installed to $(realpath ./pixel-backup-gang)"
