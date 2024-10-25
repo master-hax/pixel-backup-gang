7bf224cc867960f8fcc2c5498b9e85c3e377a4ae
Vivek <vivek@master-hax.com>
2024-10-25 00:22:11 -0700

    update install script (#24)
    * update install script
    
    * don't use set

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -eu
 
 ################################################################################
 # Description: install the latest version of pixel-backup to the local directory
@@ -6,12 +6,13 @@
 # Usage: sh -c "$(curl -fSs https://raw.githubusercontent.com/master-hax/pixel-backup-gang/master/install.sh)"
 ################################################################################
 
-pbg_tarball_url="https://github.com/master-hax/pixel-backup-gang/releases/download/0.0.2/pixel-backup-gang-0.0.2.tar.gz"
-pbg_tarball_filename="pixel-backup-gang-latest.tar.gz"
-echo "install.sh: downloading the latest release"
-curl -fL $pbg_tarball_url --output $pbg_tarball_filename
-echo "install.sh: unpacking the release archive (requires root)"
-/sbin/su --command "tar -xvf $pbg_tarball_filename"
-echo "install.sh: make the release executable (requires root)"
-/sbin/su --command "chmod +x ./pixel-backup-gang/*.sh"
+pbg_tarball_version="0.0.2"
+pbg_tarball_filename="pixel-backup-gang-$pbg_tarball_version.tar.gz"
+pbg_tarball_url="https://github.com/master-hax/pixel-backup-gang/releases/download/$pbg_tarball_version/$pbg_tarball_filename"
+echo "install.sh: downloading version $pbg_tarball_version from github"
+curl -fL $pbg_tarball_url --output $pbg_tarball_filename || { echo 'failed to download release archive' ; exit 1; }
+echo "install.sh: unpacking the release archive (requires root)" || { echo 'failed to unpack release archive' ; exit 1; }
+su --command "tar -xvf $pbg_tarball_filename" || { echo 'failed to inflate release archive' ; exit 1; }
+echo "install.sh: making the release executable (requires root)"
+su --command "chmod +x ./pixel-backup-gang/*.sh" || { echo 'failed to make scripts executable' ; exit 1; }
 echo "install.sh: pixel backup gang successfully installed to $(realpath ./pixel-backup-gang)"
