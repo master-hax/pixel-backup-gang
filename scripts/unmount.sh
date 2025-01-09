d8d00a71694cce6b7af0af7d34ac515b28177bf7
brosahay <3526930+brosahay@users.noreply.github.com>
2025-01-10 01:29:27 +0530

    Adds fixes in the unmount routine

--- scripts/unmount.sh
+++ scripts/unmount.sh
@@ -11,5 +11,19 @@ if [ "$(readlink /proc/self/ns/mnt)" != "$(readlink /proc/1/ns/mnt)" ]; then
   exit 1
 fi
 
-umount -v /mnt/runtime/write/emulated/0/the_binding
-umount -v /mnt/my_drive
+android_version=$(getprop ro.build.version.release)
+
+if [ $android_version -gt 10 ]; then
+  # for Android 11+
+  internal_binding_dir="/mnt/pass_through/0/emulated/0/the_binding"
+else
+  # for Android 10 and below
+  internal_binding_dir="/mnt/runtime/write/emulated/0/the_binding"
+fi
+
+[[ $(mount | grep -w "$internal_binding_dir") != "" ]]&& \
+  umount -v $internal_binding_dir
+
+# check if mount exists for EXT4 drives
+[[ $(mount | grep -w "/mnt/my_drive") != "" ]] && \
+  umount -v /mnt/my_drive
