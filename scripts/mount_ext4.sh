5ebc5dc170a6c81d0948c2d76e220bb35ff8c6e3
Vivek Revankar <vivek@master-hax.com>
2025-02-14 03:08:59 -0800

    improve ext4 script

--- scripts/mount_ext4.sh
+++ scripts/mount_ext4.sh
@@ -11,24 +11,46 @@ if [ "$(readlink /proc/self/ns/mnt)" != "$(readlink /proc/1/ns/mnt)" ]; then
   exit 1
 fi
 
-mkdir -p -v /mnt/my_drive
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 /dev/block/<label> e.g. $0 /dev/block/sdg1" >&2
+  exit 1
+fi
+
+ext4_blockdev_path=$1
+
+fs_type=$(stat -f -c %T "$ext4_blockdev_path")
+if [ "$fs_type" != "tmpfs" ]; then
+    echo "detected filesystem type was not 'tmpfs', found $fs_type"
+    exit 1
+fi
+
+drive_mount_dir='/mnt/my_drive'
+internal_binding_dir='/mnt/runtime/write/emulated/0/the_binding'
+
+mkdir -p -v "$drive_mount_dir"
 mount \
     -t ext4 \
     -o nosuid,nodev,noexec,noatime \
-    "$1" /mnt/my_drive
+    "$ext4_blockdev_path" "$drive_mount_dir"
 
-mkdir -p -v /mnt/my_drive/the_binding
-chmod -R 777 /mnt/my_drive
-chown -R sdcard_rw:sdcard_rw /mnt/my_drive
+mkdir -p -v "$drive_mount_dir"/the_binding
+chmod -R 777 "$drive_mount_dir"/the_binding
+chown -R sdcard_rw:sdcard_rw "$drive_mount_dir"
 
 # may be possible to avoid this by mounting to /mnt/expand/<uuid>
 # TODO: use magiskpolicy https://github.com/topjohnwu/Magisk/blob/master/docs/tools.md#magiskpolicy
 setenforce 0
 
-mkdir -p -v /mnt/runtime/write/emulated/0/the_binding
+mkdir -p -v "$internal_binding_dir"
 mount \
     -t sdcardfs \
     -o nosuid,nodev,noexec,noatime,gid=9997 \
-    /mnt/my_drive/the_binding /mnt/runtime/write/emulated/0/the_binding
+    "$drive_mount_dir/the_binding" "$internal_binding_dir"
 
 # TODO: reduce permissions via chmod & sdcardfs mask
+
+am broadcast \
+  -a android.intent.action.MEDIA_SCANNER_SCAN_FILE \
+  -d file:///storage/emulated/0/the_binding/
+
+echo "ext4 drive mounted successfully"
